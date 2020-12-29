7a868c2922ecf15b43545755aa4a66a1176e9e77
Timothy Wrona <tjwrona1992@gmail.com>
2020-12-29 15:15:04 -0500

    Updated version number

--- conanfile.py
+++ conanfile.py
@@ -14,7 +14,7 @@ class CppMusic(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     topics = ("music")
     url = "https://github.com/tnt-coders/cpp-music"
-    version = "0.0.0"
+    version = "1.0.0"
 
     def configure(self):
         tools.check_min_cppstd(self, "17")
