c8435991979845ec63851eeb735340383a76a066
Timothy Wrona <tjwrona1992@gmail.com>
2021-01-03 12:11:34 -0500

    Update Catch2 and force all find_package commands to explicitly use "CONFIG" mode

--- test/note_test.cpp
+++ test/note_test.cpp
@@ -1,5 +1,5 @@
 #include <catch2/catch_test_macros.hpp>
-#include <catch2/matchers/catch_matchers_floating.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 #include <tnt/music/note.hpp>
 #include <vector>
 
