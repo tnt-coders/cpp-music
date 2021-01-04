30226dfbb090ff0a11df8a983df8da2b407f0649
Timothy Wrona <tjwrona1992@gmail.com>
2021-01-03 22:58:29 -0500

    Reformatted conanfile

--- conanfile.py
+++ conanfile.py
@@ -1,19 +1,38 @@
 from conans import ConanFile, CMake, tools
 
 class CppMusic(ConanFile):
-    author = "TNT Coders <tnt-coders@googlegroups.com>"
-    build_requires = ["catch2/3.0.0-1@tnt-coders/stable",
-                      "math/1.0.0@tnt-coders/stable"]
-    default_options = {"shared": False}
-    description = "C++ library for storing and analyzing musical data"
-    exports_sources = "CMakeLists.txt", "docs/*", "include/*", "src/*", "test/*"
-    generators = "cmake", "cmake_paths"
-    license = "GNU Lesser General Public License v3.0"
     name = "music"
-    options = {"shared": [True, False]}
-    settings = "os", "compiler", "build_type", "arch"
-    topics = ("music")
+    description = "C++ library for storing and analyzing musical data"
+    homepage = "https://tnt-coders.github.io/"
     url = "https://github.com/tnt-coders/cpp-music"
+    license = "GNU Lesser General Public License v3.0"
+    author = "TNT Coders <tnt-coders@googlegroups.com>"
+
+    topics = ("music")
+
+    settings = ("os", "compiler", "build_type", "arch")
+
+    options = {
+        "shared": [True, False],
+    }
+
+    default_options = {
+        "shared": False,
+    }
+
+    build_requires = (
+        "catch2/3.0.0-1@tnt-coders/stable",
+        "math/1.0.0@tnt-coders/stable",
+    )
+
+    generators = ("cmake", "cmake_paths")
+
+    exports_sources = ("CMakeLists.txt", "docs/*", "include/*", "src/*", "test/*")
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.configure()
+        return cmake
 
     def configure(self):
         tools.check_min_cppstd(self, "17")
@@ -29,8 +48,3 @@ class CppMusic(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
-
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.configure()
-        return cmake
